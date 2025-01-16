d76e00867a722a652a2025e08c04bc6fcfcac589
Nisha Singh Thakuri <nisha.singhthakuri007@gmail.com>
2025-01-16 15:44:35 +0000

    Learning data types

---
+++ .devcontainer/02_datatypes.js
@@ -0,0 +1,17 @@
+let age = 22
+let state;
+let gender = null
+let isloggedin = false
+console.log(state)
+// number => 2 to power 53
+//bigint for larger numbers
+//string =>""
+//boolean =>true/false
+//null=> stand alone value 
+//undefined=>
+//symbol => unique
+
+console.log(typeof "Nisha"); //string
+console.log(typeof state);
+console.log(typeof null);
+console.log(typeof gender);
