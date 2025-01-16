8eea6715397eea768e2dfbe6918ddfe8ef70fe01
Nisha Singh Thakuri <nisha.singhthakuri007@gmail.com>
2025-01-16 15:11:11 +0000

    Learning variables

---
+++ .devcontainer/01_variables.js
@@ -0,0 +1,6 @@
+const name = "Nisha Singh Thakuri" 
+let course = "BIM"
+let city = "Kathmandu"
+/* preferred not use var because of  block scope and funcational scope
+*/
+console.table([name,course,city])
