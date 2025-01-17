16c48fd5d032b5702acee6fc0587ad3262e0558b
Nisha Singh Thakuri <nisha.singhthakuri007@gmail.com>
2025-01-17 17:33:49 +0000

    Learning Data Type Conversion

---
+++ .devcontainer/01_basics/datatypeconversion.js
@@ -0,0 +1,18 @@
+let value1 = "2"
+let value2 ="2abc"
+let value3 = null
+let value4 = undefined
+let value5 = true
+
+let valueInNumber = Number(value1)//converts to number
+let valueNumber = Number(value2)//converts to number but value is NAN
+console.log(typeof valueInNumber );
+console.log(typeof valueNumber );
+console.log(typeof value2);
+console.log(typeof value3); 
+console.log(typeof value4);
+console.log(typeof value5);//boolean 
+/*
+1=> true and 0=> false
+"" => false
+"Nisha"=> True */
