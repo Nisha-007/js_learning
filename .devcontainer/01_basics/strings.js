a202405465b2fcf999e6b83976abc43c7f2fa088
Nisha Singh Thakuri <nisha.singhthakuri007@gmail.com>
2025-01-18 16:12:23 +0000

    Learning strings

---
+++ .devcontainer/01_basics/strings.js
@@ -0,0 +1,11 @@
+let str1 = "Nisha"
+let str2 = " Singh Thakuri"
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1" +2);
+console.log("2" +1);
+console.log(1 + "2");
+let x =100
+console.log(++x);
+console.log(x++);
