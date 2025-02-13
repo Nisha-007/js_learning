288080bf28ccfe31db7024494b3a88d425cb1a3e
Nisha Singh Thakuri <nisha.singhthakuri007@gmail.com>
2025-02-13 06:40:31 +0000

    Learning json and destructing

---
+++ .devcontainer/destructing.js
@@ -0,0 +1,19 @@
+const course = {
+    coursename:"js from Nepal",
+    price:"0$",
+    courseInstructor:"hitesh"
+}
+//course.courseInstructor
+const {courseInstructor:instructor} = course//destructing object
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+//api concept
+ //{
+ //   "name":"Nisha",//keys and name both strings in json//
+//}
+[
+    {},
+    {},
+    {}//json 
